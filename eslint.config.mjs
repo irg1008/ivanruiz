6f30c8f1721eb6cc799b8e09733d17bca90e60b0
irg1008 <ivansudevlop@gmail.com>
2024-03-22 13:19:38 +0100

    chore(config): fix all lint issues and correct plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,34 @@
-import eslintConfigPrettier from 'eslint-config-prettier'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslint from '@eslint/js'
 import eslintPluginAstro from 'eslint-plugin-astro'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import tseslint from 'typescript-eslint'
+
+const compat = new FlatCompat()
 
 /**
- * @type {import("eslint").Linter.FlatConfig} F
+ * @type {import("eslint").Linter.FlatConfig}
  */
 export default [
+	eslint.configs.recommended,
+	eslintPluginPrettierRecommended,
+	...tseslint.configs.recommended,
+	...eslintPluginAstro.configs['flat/recommended'],
 	...eslintPluginAstro.configs['flat/jsx-a11y-recommended'],
-	eslintConfigPrettier,
-	{
-		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
+	...compat.config({
+		extends: ['plugin:tailwindcss/recommended'],
+		rules: {
+			'tailwindcss/no-custom-classname': 'warn',
+			'tailwindcss/classnames-order': 'error',
 		},
-		rules: {},
+	}),
+	{
+		rules: {
+			'no-console': 'warn',
+			'no-extend-native': 'error',
+			'no-implicit-coercion': 'error',
+			'prefer-const': 'error',
+			'no-var': 'error',
+		},
 	},
 ]
--- src/components/reactflow/index.tsx
+++ src/components/reactflow/index.tsx
@@ -23,7 +23,7 @@ const initialNodes: Node[] = [
 const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2', animated: true }]
 
 export function ReactFlowCanvas() {
-	const [nodes, _, onNodesChange] = useNodesState(initialNodes)
+	const [nodes, , onNodesChange] = useNodesState(initialNodes)
 	const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
 
 	const onConnect = useCallback(
