5da84407546f3ae65f2a479c9a9f09ad91c28b68
irg1008 <ivansudevlop@gmail.com>
2024-03-22 04:54:58 +0100

    feat(nodes): add types

--- src/components/reactflow/index.tsx
+++ src/components/reactflow/index.tsx
@@ -1,20 +1,26 @@
 import { useCallback } from 'react'
 import ReactFlow, {
+	Background,
+	BackgroundVariant,
+	Controls,
+	MiniMap,
+	ReactFlowProvider,
 	addEdge,
 	useEdgesState,
 	useNodesState,
 	type Connection,
 	type Edge,
+	type Node,
 } from 'reactflow'
 
 import 'reactflow/dist/style.css'
 import './style.module.css'
 
-const initialNodes = [
-	{ id: '1', position: { x: 10, y: 50 }, data: { label: '1' } },
-	{ id: '2', position: { x: 10, y: 200 }, data: { label: '2' } },
+const initialNodes: Node[] = [
+	{ id: '1', position: { x: 0, y: 50 }, data: { label: '1' } },
+	{ id: '2', position: { x: 0, y: 200 }, data: { label: '2' } },
 ]
-const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }]
+const initialEdges: Edge[] = [{ id: 'e1-2', source: '1', target: '2', animated: true }]
 
 export function ReactFlowCanvas() {
 	const [nodes, _, onNodesChange] = useNodesState(initialNodes)
@@ -27,13 +33,20 @@ export function ReactFlowCanvas() {
 
 	return (
 		<div className='h-screen w-screen'>
-			<ReactFlow
-				nodes={nodes}
-				edges={edges}
-				onNodesChange={onNodesChange}
-				onEdgesChange={onEdgesChange}
-				onConnect={onConnect}
-			/>
+			<ReactFlowProvider>
+				<ReactFlow
+					fitView
+					nodes={nodes}
+					edges={edges}
+					onNodesChange={onNodesChange}
+					onEdgesChange={onEdgesChange}
+					onConnect={onConnect}
+				>
+					<MiniMap />
+					<Controls />
+					<Background variant={BackgroundVariant.Dots} />
+				</ReactFlow>
+			</ReactFlowProvider>
 		</div>
 	)
 }
