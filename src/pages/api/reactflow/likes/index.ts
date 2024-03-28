3ffba4a23f844b0c42f9d0724431ed1aae68ccdc
irg1008 <ivansudevlop@gmail.com>
2024-03-28 01:21:36 +0100

    build: revert put change -> not working

--- src/pages/api/reactflow/likes/index.ts
+++ src/pages/api/reactflow/likes/index.ts
@@ -2,7 +2,7 @@ import type { CreateNodeLikeDTO } from '@/db/dto/reactflow.dto'
 import { createLike, getAllLikesCount } from '@/pages/_services/reactflow.service'
 import type { APIRoute } from 'astro'
 
-export const PUT: APIRoute = async (ctx) => {
+export const POST: APIRoute = async (ctx) => {
 	const body: CreateNodeLikeDTO = await ctx.request.json()
 
 	const nodeLike = await createLike(body)
