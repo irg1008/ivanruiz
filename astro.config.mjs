1313545112e9e122068afb6774da351501b4e16b
irg1008 <ivansudevlop@gmail.com>
2024-03-31 06:16:15 +0200

    chore: config and assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,41 +7,41 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://astro.build/config
 export default defineConfig({
-	prefetch: true,
-	devToolbar: {
-		enabled: false,
-	},
-	integrations: [
-		tailwind({
-			nesting: true,
-			applyBaseStyles: true,
-		}),
-		react(),
-		sitemap(),
-	],
-	output: 'server',
-	adapter: cloudflare(),
-	build: {
-		inlineStylesheets: 'always',
-	},
-	vite: {
-		build: {
-			cssMinify: 'lightningcss',
-		},
-		ssr: {
-			external: ['node:buffer', 'node:crypto'],
-		},
-		plugins: [
-			VitePWA({
-				registerType: 'autoUpdate',
-				includeAssets: ['**/*'],
-				injectRegister: 'auto',
-				workbox: {
-					globDirectory: 'dist',
-					globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
-					navigateFallback: null,
-				},
-			}),
-		],
-	},
+  prefetch: true,
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [
+    tailwind({
+      nesting: true,
+      applyBaseStyles: true,
+    }),
+    react(),
+    sitemap(),
+  ],
+  output: 'server',
+  adapter: cloudflare(),
+  build: {
+    inlineStylesheets: 'always',
+  },
+  vite: {
+    build: {
+      cssMinify: 'lightningcss',
+    },
+    ssr: {
+      external: ['node:buffer', 'node:crypto'],
+    },
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['**/*'],
+        injectRegister: 'auto',
+        workbox: {
+          globDirectory: 'dist',
+          globPatterns: ['**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}'],
+          navigateFallback: null,
+        },
+      }),
+    ],
+  },
 })
