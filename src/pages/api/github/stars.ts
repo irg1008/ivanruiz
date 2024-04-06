47ea88a82ac7d5daddf0d93c601a3891769b9ade
irg1008 <ivansudevlop@gmail.com>
2024-04-06 16:18:20 +0200

    chore(build): test cron job

--- src/pages/api/github/stars.ts
+++ src/pages/api/github/stars.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from 'astro'
 
 export const GET: APIRoute = async () => {
+  console.log('holi')
+
   return new Response(JSON.stringify({ message: 'Hello, World!' }), {
     headers: {
       'content-type': 'application/json',
